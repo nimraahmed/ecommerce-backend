968df1e6d150b41322e014da2af46b8fadf75c2c
Nimra Ahmed <50912134+nimraahmed@users.noreply.github.com>
2021-05-18 16:37:59 +0500

    Add files via upload

---
+++ models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: String,
+    required: true,
+  },
+  img:
+    {
+      name: {
+        type: String,
+      },
+      path: {
+        type: String,
+      },
+      type: {
+        type: String,
+      },
+    },
+});
+
+module.exports = mongoose.model("Product", productSchema);
